2327d918c496a3954792858e084f30a163cda345
Tim Caswell <tim@creationix.com>
2016-08-18 21:08:10 -0500

    Move msgpack to it's own repo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import { sha3_256 } from "./sha3";
+// import { sha3_256 } from "./sha3";
 // import { run } from "./async";
-
-import { register, encode, decode } from "./msgpack";
+import { register } from "./msgpack";
 
 // Register the Link type so we can serialize hashes as a new special type.
 // hash itself is just a 32 byte Uint8Array
@@ -12,41 +11,3 @@ register(127, Link,
   (link) => { return link.hash; },
   (buf) => { return new Link(buf); }
 );
-
-let hash = sha3_256.buffer("Hello World");
-let link = new Link(hash);
-
-
-let tests = [
-  true,
-  false,
-  null,
-  "Hello",
-  [1, 2, 3],
-  link,
-  new Uint8Array([1,2,3]),
-  {name:"Tim"},
-  {
-    isProgrammer: true,
-    badParts: null,
-    name: "Tim",
-    age: 34,
-    message: link
-  },
-  [1,-1,100,-100,10000,-10000, 100000000, -100000000, 10000000000, -1000000000]
-];
-
-function serialize(value) {
-  return Object.prototype.toString.call(value)+value;
-}
-
-for (let test of tests) {
-  console.log("Expected", test);
-  let encoded = encode(test);
-  console.log("Encoded", encoded);
-  let decoded = decode(encoded);
-  console.log("Actual", decoded);
-  if (serialize(test) !== serialize(decoded)) {
-    throw new Error("MISMATCH");
-  }
-}
