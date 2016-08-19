0e908864e3864411c482a753258bdde66c026ae8
Tim Caswell <tim@creationix.com>
2016-08-18 21:31:52 -0500

    Add idb-keyval

--- rollup.config.js
+++ rollup.config.js
@@ -3,4 +3,5 @@ export default {
   entry: 'src/main.js',
   dest: 'www/bundle.js',
   format: 'iife',
+  sourceMap: true
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import { sha3_256 } from "./sha3";
-// import { run } from "./async";
-import { register } from "./msgpack";
+import { run } from "./async";
+import { register, encode, decode } from "./msgpack";
+import { idbKeyval as storage } from "./idb-keyval";
 
 // Register the Link type so we can serialize hashes as a new special type.
 // hash itself is just a 32 byte Uint8Array
@@ -11,3 +12,9 @@ register(127, Link,
   (link) => { return link.hash; },
   (buf) => { return new Link(buf); }
 );
+
+run(function*() {
+  yield storage.set("test", encode([1,2,3]));
+  let out = decode(yield storage.get("test"));
+  console.log(out);
+}())
