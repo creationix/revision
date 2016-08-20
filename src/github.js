8ee1194dd42ec13252d6183b02574271ab6728c5
Tim Caswell <tim@creationix.com>
2016-08-19 22:23:59 -0500

    More data model cleanup

--- src/github.js
+++ src/github.js
@@ -3,20 +3,20 @@ import { runAll } from "./async";
 import { idbKeyval as storage } from "./idb-keyval";
 
 let modeToType = {
-   '40000': 0, // tree
-  '040000': 0, // tree
-  '100644': 1, // blob
-  '100755': 2, // exec
-  '120000': 3, // sym
-  '160000': 4  // commit
+   '40000': "tree",
+  '040000': "tree",
+  '100644': "file",
+  '100755': "exec",
+  '120000': "sym",
+  '160000': "sub"
 };
-let modeToImport = {
-   '40000': importTree, // tree
-  '040000': importTree, // tree
-  '100644': importBlob, // blob
-  '100755': importBlob, // exec
-  '120000': importBlob, // sym
-  '160000': importSubmodule  // commit
+let modeToRead = {
+   '40000': readTree, // tree
+  '040000': readTree, // tree
+  '100644': readBlob, // blob
+  '100755': readExec, // exec
+  '120000': readSym, // sym
+  '160000': readSubmodule  // commit
 }
 
 
@@ -38,7 +38,7 @@ function* get(path, format) {
   return res && (yield res[format]());
 }
 
-export function* deref(owner, repo, ref) {
+function* deref(owner, repo, ref) {
   if (/^[0-9a-f]{40}$/.test(ref)) return ref;
   let result = yield* get(`repos/${owner}/${repo}/git/refs/${ref}`);
   return result && result.object.sha;
@@ -52,15 +52,21 @@ function* gitLoad(owner, repo, type, sha) {
     type === "blob" ? "arrayBuffer" : "json"
   );
   if (!result) return;
+  if (type === "blob") result = new Uint8Array(result);
   yield storage.set(sha, result);
   return result;
 }
 
-function parseGitmodules(arr) {
-  let text = "";
-  for (let i = 0, l = arr.length; i <l; i++) {
-    text += String.fromCharCode(arr[i]);
+function bufToString(buf) {
+  let str = "";
+  for (let i = 0, l = buf.length; i <l; i++) {
+    str += String.fromCharCode(buf[i]);
   }
+  return str;
+}
+
+function parseGitmodules(buf) {
+  let text = bufToString(buf);
   let config = {};
   let section;
   text.split(/[\r\n]+/).forEach(function (line) {
@@ -80,66 +86,65 @@ function parseGitmodules(arr) {
   return config;
 }
 
-export function* importBlob(owner, repo, sha) {
-  sha = yield* deref(owner, repo, sha);
-  let data = yield* gitLoad(owner, repo, "blob", sha);
-  return yield* save(data);
+function* readSym(owner, repo, sha) {
+  let buf = yield* gitLoad(owner, repo, "blob", sha);
+  return bufToString(buf);
 }
 
-export function* importTree(owner, repo, sha, path, gitmodules) {
-  sha = yield* deref(owner, repo, sha);
+function* readExec(owner, repo, sha) {
+  let buf = yield* gitLoad(owner, repo, "blob", sha);
+  // TODO: encode exec bit somehow
+  return yield* save(buf);
+}
+
+function* readBlob(owner, repo, sha) {
+  let buf = yield* gitLoad(owner, repo, "blob", sha);
+  return yield* save(buf);
+}
+
+export function* readTree(owner, repo, sha, path, gitmodules) {
   let result = yield* gitLoad(owner, repo, "tree", sha);
   let tasks = [];
   for (let entry of result.tree) {
     if (!gitmodules && entry.path === ".gitmodules") {
       gitmodules = parseGitmodules(
-        new Uint8Array(yield* gitLoad(owner, repo, "blob", entry.sha))
+        yield* gitLoad(owner, repo, "blob", entry.sha)
       );
     }
     let newPath = path ? `${path}/${entry.path}` : entry.path;
-    tasks.push(modeToImport[entry.mode](
+    tasks.push(modeToRead[entry.mode](
       owner, repo, entry.sha, newPath, gitmodules
     ));
   }
-  let tree = (yield runAll(tasks)).map(function (link, i) {
+  let tree = {};
+  (yield runAll(tasks)).forEach(function (item, i) {
     let entry = result.tree[i];
-    return [
-      modeToType[entry.mode],
-      entry.path,
-      link
-    ];
+    tree[entry.path] = item;
   });
-  return yield* save(tree);
+  return tree;
 }
 
-export function* importCommit(owner, repo, sha) {
+export function* readCommit(owner, repo, sha) {
   sha = yield* deref(owner, repo, sha);
-  let result = yield* gitLoad(owner, repo, "commit", sha);
-  let commit = {
-    url: result.url,
-    root: yield* importTree(owner, repo, result.tree.sha)
+  let commit = yield* gitLoad(owner, repo, "commit", sha);
+  let tree = yield* readTree(owner, repo, commit.tree.sha);
+  return {
+    github: `${owner}/${repo}`,
+    sha: sha,
+    tree: yield* save(tree)
   };
-  // Uncomment to include all parent commits.
-  // if (result.parents) {
-  //   let parents = commit.parents = [];
-  //   for (let parent of result.parents) {
-  //     parents.push(yield* importCommit(owner, repo, parent.sha));
-  //   }
-  // }
-  return yield* save(commit);
 }
 
-export function* importSubmodule(owner, repo, sha, path, gitmodules) {
+function* readSubmodule(owner, repo, sha, path, gitmodules) {
   let remote;
   for (let key in gitmodules.submodule) {
     let sub = gitmodules.submodule[key];
-    if (sub.path === path) {
-      remote = sub.url;
-      break;
-    }
+    if (sub.path !== path) continue;
+    remote = sub.url;
+    break;
   }
   if (!remote) throw new Error(`No gitmodules entry for ${path}`);
   let match = remote.match(/github.com[:\/]([^\/]+)\/(.+?)(\.git)?$/);
   if (!match) throw new Error(`Submodule is not on github ${remote}`);
-  return yield* importCommit(match[1], match[2], sha);
+  return yield* readCommit(match[1], match[2], sha);
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import { run } from "./async";
-import { load } from "./cas";
-import { importCommit } from "./github";
+import { readCommit } from "./github";
 
 run(function*() {
   let owner = "creationix";
   let repo = "revision";
   let ref = "heads/master";
   console.log(`Importing github://${owner}/${repo}/refs/${ref}`);
-  let link = yield* importCommit(owner, repo, ref);
-  console.log(`Imported as ${link.toHex()}`);
-  console.log(yield* load(link));
+  let commit = yield* readCommit(owner, repo, ref);
+  console.log(commit);
+  let tree = yield* commit.tree.resolve();
+  console.log(tree);
 }());
