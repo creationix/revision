181a27757d72d33479d0c0337c61eb4fab481d67
Tim Caswell <tim@creationix.com>
2016-08-30 19:14:30 -0500

    Implement server-side CAS and use bintools in link class

--- src/libs/link.js
+++ src/libs/link.js
@@ -1,17 +1,11 @@
 import { register, encode, decode } from "./msgpack";
 import { sha1 } from "./sha1";
+import { binToHex, hexToBin } from "./bintools";
 
 // Consumers of this API must provide the following interface here.
 // function get(hash) -> promise<value>
 // function set(hash, value) -> promise
 export let storage = {};
-
-// Register the Link type so we can serialize hashes as a new special type.
-// hash itself is just a 20 byte Uint8Array
-register(127, Link,
-  (link) => { return link.hash; },
-  (buf) => { return new Link(buf); }
-);
 
 // Save takes a value and serializes and stores it returning the link.
 export function* save(value) {
@@ -29,40 +23,32 @@ export function* load(link) {
 }
 
 // Link has some nice methods in addition to storing the hash buffer.
-export function Link(hash) {
-  if (hash.constructor === ArrayBuffer) {
-    hash = new Uint8Array(hash);
-  }
-  if (hash.constructor === Uint8Array) {
-    this.hash = hash;
-    return;
-  }
-  if (typeof hash === "string") {
-    if (!/^[0-9a-f]{40}$/.test(hash)) {
-      throw new TypeError("Invalid string, expected hash");
+export class Link {
+  constructor(hash) {
+    if (hash.constructor === ArrayBuffer) hash = new Uint8Array(hash);
+    if (hash.constructor === Uint8Array) {
+      this.hash = hash;
+      return;
     }
-    this.hash = new Uint8Array(20);
-    let j = 0;
-    for (let i = 0; i < 40; i += 2) {
-      this.hash[j++] = parseInt(hash.substr(i, 2), 16);
+    if (typeof hash === "string") {
+      if (!/^[0-9a-f]{40}$/.test(hash)) {
+        throw new TypeError("Invalid string, expected hash");
+      }
+      this.hash = hexToBin(hash);
+      return;
     }
-    return;
+    throw new TypeError("Invalid hash, expected string or buffer");
   }
-  throw new TypeError("Invalid hash, expected string or buffer");
+  *resolve() {
+    return yield* load(this);
+  }
+  toHex() {
+    return binToHex(this.hash);
+  }
+  toBin() {
+    return this.hash;
+  }
 }
-Link.prototype.resolve = function* resolve() {
-  return yield* load(this);
-};
-Link.prototype.toHex = function toHex() {
-  let hex = "";
-  let buf = this.hash;
-  for (let i = 0, l = buf.length; i < l; i++) {
-    let byte = buf[i];
-    hex += (byte < 0x10 ? "0" : "") + byte.toString(16);
-  }
-  if (!hex) throw new Error("WAT")
-  return hex;
-};
 
 // Look for links in an object
 export function scan(value, onLink) {
@@ -78,3 +64,10 @@ export function scan(value, onLink) {
     }
   }
 }
+
+// Register the Link type so we can serialize hashes as a new special type.
+// hash itself is just a 20 byte Uint8Array
+register(127, Link,
+  (link) => { return link.hash; },
+  (buf) => { return new Link(buf); }
+);
--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
-
+import { load, save } from "./libs/cas-mem";
 import { Server, autoHeaders, logger, files } from "./libs/weblit";
 
 new Server()
   .use(logger)      // To log requests to stdout
   .use(autoHeaders) // To ensure we send proper HTTP headers
   .use(files("."))  // To serve up the client-side app
-  .route({          // To handle logic
-    method: "GET",
-    path: "/:name"
-  }, function* (req, res) {
+
+  // Serve objects over GET requests
+  .route({ method: "GET", path: "/:hash"}, function* (req, res, next) {
+    let hash = req.params.hash;
+    if (!/^[0-9a-f]{40}$/.test(hash)) return yield* next();
     res.code = 200;
-    res.body = `Hi ${req.params.name}!`;
+    res.headers.set("Content-Type", "application/octet-stream");
+    res.body = yield* load(hash);
   })
+
+  // Handle object uploads over POST
+  .route({ method: "POST", path: "/"}, function* (req, res) {
+    let link = yield* save(req.body);
+    res.code = 200;
+    res.headers.set("Content-Type", "application/octet-stream");
+    res.headers.set("Location", `/${link.toHex()}`);
+    res.body = link.toBin();
+  })
+
   .start();
